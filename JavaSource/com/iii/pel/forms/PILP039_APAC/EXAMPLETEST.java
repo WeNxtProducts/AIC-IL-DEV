884f576f8d2014c316c001255b3c7bd26964a33b
WenxtTech <timothygodwin@wenxttech.com>
2024-11-12 16:50:11 +0530

    first commit

---
+++ JavaSource/com/iii/pel/forms/PILP039_APAC/EXAMPLETEST.java
@@ -0,0 +1,11 @@
+package com.iii.pel.forms.PILP039_APAC;
+
+
+
+public class EXAMPLETEST 
+{
+	public static void main(String[] args) {
+		String polsysId = "String polsysId";
+		polsysId = polsysId.substring(polsysId.lastIndexOf("&") + 1);
+	}
+}
